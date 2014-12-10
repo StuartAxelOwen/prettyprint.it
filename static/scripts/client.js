dd5fa91d293e93aa03d3f5f2a536eec24f09b40e
Stuart Axel Owen <stuart.owen@gmail.com>
2014-12-09 19:44:59 -0800

    first commit of pretty print - adding express web server and basic json beautification

---
+++ static/scripts/client.js
@@ -0,0 +1,27 @@
+// client.js
+
+var prettyprint = (function(document, window) {
+  var inEle = document.getElementById('text-input');
+  var outEle = document.getElementById('text-output');
+
+  (function stretchTextAreas() {
+    var header = document.getElementById('header');
+    var textAreaHeight = (window.innerHeight || document.documentElement.clientHeight) - header.offsetHeight - 5;
+    inEle.style.height = textAreaHeight;
+    outEle.style.height = textAreaHeight;
+  })();
+  inEle.focus();
+
+  function render() {
+    renderJSON();
+  }
+
+  function renderJSON() {
+    outEle.value = JSON.stringify(JSON.parse(inEle.value), undefined, 2);
+  }
+
+  return {
+    renderJSON: renderJSON,
+    render: render
+  };
+})(document, window);
